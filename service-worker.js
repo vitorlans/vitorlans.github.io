eb2464500d0f3c3b0e045d016a2a3ad97d6d4afb
Vitor Lans <vitor_hs@live.com>
2018-03-09 02:23:42 -0300

    deploy 18.03

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ function setOfCachedUrls(e) {
   }).then(function (e) {
     return new Set(e);
   });
-}var precacheConfig = [["/index.html", "7e082af5b0493759712cf533670f9aaa"], ["/static/css/main.0f31b4eb.css", "4b25f06732e9923cc8db0dcf31f99369"], ["/static/js/main.3547f7a8.js", "5e3ab607c6d113c262fa319097640638"], ["/static/media/SamsungOne-300.bae10944.woff", "bae10944d6e91f054942052293699fb6"], ["/static/media/SamsungOne-400.c6d604a8.woff", "c6d604a8322eea608e318f942462ee94"], ["/static/media/SamsungOne-600.6a756b3c.woff", "6a756b3c772f0a81378ac1c12777e17a"], ["/static/media/SamsungOne-700.6155d98a.woff", "6155d98a83b5cd468bb535ac4fb8fd14"], ["/static/media/SamsungOne-800.d37ce275.woff", "d37ce275e272185348ea4d43223dbb50"]],
+}var precacheConfig = [["/index.html", "a9f84d00c9687ac5ea78451bceb60fc4"], ["/static/css/main.0f31b4eb.css", "4b25f06732e9923cc8db0dcf31f99369"], ["/static/js/main.d3b39c22.js", "8e2dd092eecf96d491de9c5a49b9c412"], ["/static/media/SamsungOne-300.bae10944.woff", "bae10944d6e91f054942052293699fb6"], ["/static/media/SamsungOne-400.c6d604a8.woff", "c6d604a8322eea608e318f942462ee94"], ["/static/media/SamsungOne-600.6a756b3c.woff", "6a756b3c772f0a81378ac1c12777e17a"], ["/static/media/SamsungOne-700.6155d98a.woff", "6155d98a83b5cd468bb535ac4fb8fd14"], ["/static/media/SamsungOne-800.d37ce275.woff", "d37ce275e272185348ea4d43223dbb50"]],
     cacheName = "sw-precache-v3-sw-precache-webpack-plugin-" + (self.registration ? self.registration.scope : ""),
     ignoreUrlParametersMatching = [/^utm_/],
     addDirectoryIndex = function (e, t) {
